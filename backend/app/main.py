7f0d9f58a87a0098ba6349634a92bbfa1fdc8812
Daniel Nery <danielpontesnery@gmail.com>
2026-06-24 11:59:31 -0300

    fix: cors origin frontned.

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,6 +21,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://master.d2lr5f2wgywxvc.amplifyapp.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
